782a0c80e26d8f39ae4723156f7572b93e10720e
BLACK <71935902+BLACK4585@users.noreply.github.com>
2023-05-06 22:06:22 +0200

    Added extra box, changed rotation and added a car

--- src/main/java/tk/blackdev/ValoLand.java
+++ src/main/java/tk/blackdev/ValoLand.java
@@ -1,15 +1,19 @@
 package tk.blackdev;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.AmbientLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 /**
- * This is the Main Class of your Game. It should boot up your game and do initial initialisation
- * Move your Logic into AppStates or Controls or other java classes
+ * This is the Main Class of your Game. It should boot up your game and do
+ * initial initialisation Move your Logic into AppStates or Controls or other
+ * java classes
  */
 public class ValoLand extends SimpleApplication {
 
@@ -22,17 +26,39 @@ public class ValoLand extends SimpleApplication {
     public void simpleInitApp() {
         Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
-
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Blue);
         geom.setMaterial(mat);
 
-        rootNode.attachChild(geom);
+        Box b2 = new Box(1, 1, 1);
+        Geometry geom2 = new Geometry("Box2", b2);
+        Material mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat2.setColor("Color", ColorRGBA.Orange);
+        geom2.setMaterial(mat2);
+        geom2.setLocalTranslation(2, 2, 0);
+
+        Node pivot = new Node("pivot");
+        pivot.attachChild(geom);
+        pivot.attachChild(geom2);
+        
+        Spatial car = assetManager.loadModel("Models/bugatti.j3o");
+        Material mat3 = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
+        car.setMaterial(mat3);
+        car.setLocalTranslation(-5, -5, 0);
+        car.rotate(0, 180, 0);
+
+        AmbientLight light = new AmbientLight(ColorRGBA.White);
+        
+        rootNode.attachChild(pivot);
+        rootNode.attachChild(car);
+        rootNode.addLight(light);
+
+        pivot.rotate(.4f, .4f, .0f);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //this method will be called every game tick and can be used to make updates
+        //comment
     }
 
     @Override
