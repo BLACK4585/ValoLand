41098c6c36efae07c390a1deb82954ecbc535565
BLACK <71935902+BLACK4585@users.noreply.github.com>
2023-05-06 22:50:00 +0200

    Tried adding pulsing box

--- src/main/java/tk/blackdev/ValoLand.java
+++ src/main/java/tk/blackdev/ValoLand.java
@@ -1,9 +1,11 @@
 package tk.blackdev;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.ZipLocator;
 import com.jme3.light.AmbientLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
@@ -16,6 +18,10 @@ import com.jme3.scene.shape.Box;
  * java classes
  */
 public class ValoLand extends SimpleApplication {
+    
+    private Spatial car;
+    private Geometry geom2;
+    private boolean grow = true;
 
     public static void main(String[] args) {
         ValoLand app = new ValoLand();
@@ -31,7 +37,7 @@ public class ValoLand extends SimpleApplication {
         geom.setMaterial(mat);
 
         Box b2 = new Box(1, 1, 1);
-        Geometry geom2 = new Geometry("Box2", b2);
+        geom2 = new Geometry("Box2", b2);
         Material mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat2.setColor("Color", ColorRGBA.Orange);
         geom2.setMaterial(mat2);
@@ -41,7 +47,7 @@ public class ValoLand extends SimpleApplication {
         pivot.attachChild(geom);
         pivot.attachChild(geom2);
         
-        Spatial car = assetManager.loadModel("Models/bugatti.j3o");
+        car = assetManager.loadModel("Models/bugatti.j3o");
         Material mat3 = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
         car.setMaterial(mat3);
         car.setLocalTranslation(-5, -5, 0);
@@ -49,16 +55,34 @@ public class ValoLand extends SimpleApplication {
 
         AmbientLight light = new AmbientLight(ColorRGBA.White);
         
+        assetManager.registerLocator("town.zip", ZipLocator.class);
+        Spatial gameLevel = assetManager.loadModel("main.scene");
+        gameLevel.setLocalTranslation(0, -10, 90);
+        gameLevel.setLocalScale(2);
+        
         rootNode.attachChild(pivot);
         rootNode.attachChild(car);
         rootNode.addLight(light);
+        rootNode.attachChild(gameLevel);
 
         pivot.rotate(.4f, .4f, .0f);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //comment
+        car.rotate(0, 2*tpf, 0);
+        if (grow) {
+            geom2.scale(1 + (3f * tpf));
+        } else {
+            geom2.scale(1 - (3f * tpf));
+        }
+
+        Vector3f s = geom2.getLocalScale();
+        if (s.getX() > 1.2f) {
+            grow = false;
+        } else if (s.getX() < 0.8f) {
+            grow = true;
+        }
     }
 
     @Override
